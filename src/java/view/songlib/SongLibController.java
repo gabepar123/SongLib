88879eb7a31b06a2ee6d7e9734f3ae24c3086000
Mohamed Amine Smires <mohamedamines@outlook.com>
2022-02-07 15:25:14 -0500

    Working Delete Button
    Delete Button deletes song. Made s for selected song global to support this.

--- src/java/view/songlib/SongLibController.java
+++ src/java/view/songlib/SongLibController.java
@@ -37,6 +37,7 @@ public class SongLibController {
 
     @FXML private Label selectedYear;
 
+    Song s; // made global to support removal
 
 
 
@@ -66,6 +67,14 @@ public class SongLibController {
     @FXML
     protected void onDeleteButtonClick(ActionEvent event){ //TODO: delete song from library
         System.out.println("Delete Button was clicked!");
+
+        songList.remove(s); //remove the song from the list
+        updateSongList(); //update the listView
+
+        //clear the labels for the selected song
+        selectedSong.setText("");
+        selectedArtist.setText("");
+
     }
 
     @FXML
@@ -74,7 +83,7 @@ public class SongLibController {
     }
     @FXML
     protected void onSongListViewClick(MouseEvent event){
-        Song s = songListView.getSelectionModel().getSelectedItem();
+        s = songListView.getSelectionModel().getSelectedItem();
         if (s == null) return; //if there is no songs in the list
         selectedSong.setText(s.name);
         selectedArtist.setText(s.artist);
